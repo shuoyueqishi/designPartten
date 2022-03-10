987f545837c1dba38a83c1daa91b5553502fe45d
xlt <2219321592@qq.com>
2022-03-10 23:32:47 +0800

    yyyy

--- src/observer/exchangeRate/AbstractRate.java
+++ src/observer/exchangeRate/AbstractRate.java
@@ -7,13 +7,17 @@ import java.util.List;
 public abstract class AbstractRate {
     protected List<ICompany> companyList = new ArrayList<>();
     private BigDecimal base = new BigDecimal("6.8");
+
     void addCompany(ICompany company) {
         this.companyList.add(company);
     }
+
     void removeCompany(ICompany company) {
         this.companyList.remove(company);
     }
+
     abstract void change(BigDecimal value);
+
     public BigDecimal getBase() {
         return this.base;
     }
--- src/observer/exchangeRate/RateClient.java
+++ src/observer/exchangeRate/RateClient.java
@@ -2,12 +2,9 @@ package observer.exchangeRate;
 
 import java.math.BigDecimal;
 
-public class Test {
+public class RateClient {
     public static void main(String[] args ) {
         RmbRate rmbRate = new RmbRate();
-        rmbRate.addCompany(new ImportCompany("华为"));
-        rmbRate.addCompany(new ImportCompany("OPPO"));
-        rmbRate.addCompany(new ImportCompany("小米"));
         rmbRate.addCompany(new ExportCompany("腾讯"));
         rmbRate.addCompany(new ExportCompany("阿里巴巴"));
         rmbRate.addCompany(new ExportCompany("美团"));
